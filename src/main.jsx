9a02a8bf6c4ed8d61d63736fcbb41a1462b7b5f4
thazin naing <thazinaingmm@gmail.com>
2023-08-09 11:28:19 +0630

    create users with thunk

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './app/store.jsx'
+import { fetchUsers } from './features/users/userSlices.jsx'
+
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
