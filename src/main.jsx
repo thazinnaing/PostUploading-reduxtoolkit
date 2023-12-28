2abb2c36cbad0dc0fec920008a9df1dd6e7abcef
Thazin <thazinnaingmm@gmail.com>
2023-12-28 15:49:22 +0630

    feat: install and import

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,20 @@ import { Provider } from "react-redux";
 import { store } from "./app/store.jsx";
 import { fetchUsers } from "./features/users/userSlices.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <BrowserRouter>
-          <App/>
+          <App />
         </BrowserRouter>
       </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
