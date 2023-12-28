2809ed0323cc2c0fc5ae80ac061eb0be2c67b7d9
Thazin <thazinnaingmm@gmail.com>
2023-12-28 15:39:56 +0630

    fix: remove query client

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,15 @@ import { Provider } from "react-redux";
 import { store } from "./app/store.jsx";
 import { fetchUsers } from "./features/users/userSlices.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient,  QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
 
 store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <BrowserRouter>
           <App/>
         </BrowserRouter>
       </Provider>
-    </QueryClientProvider>
   </React.StrictMode>
 );
